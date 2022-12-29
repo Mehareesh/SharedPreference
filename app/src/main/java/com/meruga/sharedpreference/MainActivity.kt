8dd336f549ce56e53df2be78f4c68f35c29eaabc
Mehareesh Meruga <mehareeshm@gmail.com>
2022-12-29 22:29:55 +0530

    Logic to save and load data with SharedPreferences

--- app/src/main/java/com/meruga/sharedpreference/MainActivity.kt
+++ app/src/main/java/com/meruga/sharedpreference/MainActivity.kt
@@ -2,10 +2,38 @@ package com.meruga.sharedpreference
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val sharedPreferences = getSharedPreferences("my_pref", MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+
+        save_btn.setOnClickListener {
+            val name = name_et.text.toString()
+            val age = age_et.text.toString().toInt()
+            val isAdult = adult_cb.isChecked
+
+            editor.apply {
+                putString("name", name)
+                putInt("age", age)
+                putBoolean("isAdult", isAdult)
+                apply()
+            }
+        }
+
+        load_btn.setOnClickListener {
+            val name = sharedPreferences.getString("name", null)
+            val age = sharedPreferences.getInt("age", 0)
+            val isAdult = sharedPreferences.getBoolean("isAdult", false)
+
+            name_et.setText(name)
+            age_et.setText(age.toString())
+            adult_cb.isChecked = isAdult
+        }
     }
 }
